bde5cd8f102bb451c0965d232392f3eeb6bf8b7a
Bichntn <37411018+NgocBich2012@users.noreply.github.com>
2023-08-19 17:49:18 +0700

    Update test1.spec.js

--- tests/test1.spec.js
+++ tests/test1.spec.js
@@ -3,7 +3,7 @@ const { test, expect } = require('@playwright/test');
 
 test.describe("suite 2 worker 2",() => {
     test("test 2", async ({ page }) => {
-        const num = 3
+        const num = 4
         console.log(num)
     })
 })
