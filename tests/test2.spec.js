a4c7fc31a0a4b4a7372ed2eb8aad04b03ca2af0a
Bichntn <37411018+NgocBich2012@users.noreply.github.com>
2023-08-19 17:54:12 +0700

    Update test2.spec.js

--- tests/test2.spec.js
+++ tests/test2.spec.js
@@ -1,6 +1,10 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
+/**
+ * @worker: 2
+ */
+
 test.describe("suite 1 worker 1",() => {
     test("test 1", async ({ page }) => {
         const num = 1
